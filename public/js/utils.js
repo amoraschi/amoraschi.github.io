a822b195acebe0b7e840e0a8ed6d692e5e272783
DrMoraschi <68395930+amoraschi@users.noreply.github.com>
2022-06-07 16:49:05 +0200

    Some changes

--- public/js/utils.js
+++ public/js/utils.js
@@ -1,2 +1,17 @@
-;(async () => {
-})()
+function changeTabcontents (name) {
+  console.log(name)
+  const tabcontents = document.getElementsByClassName('tab-content')
+  for (const tabcontent of tabcontents) {
+    tabcontent.style.display = 'none'
+  }
+
+  const tabToShow = document.querySelector(`#${name}`)
+  if (tabToShow == null) return
+  if (tabToShow.id.includes('home')) {
+    tabToShow.style.display = 'flex'
+  } else {
+    tabToShow.style.display = 'block'
+  }
+}
+
+console.log(changeTabcontents)
