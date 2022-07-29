b543d64cf8eda45d71386a3842af4c353979811f
DrMoraschi <68395930+DrMoraschi@users.noreply.github.com>
2022-07-29 12:04:41 +0200

    More changes

--- src/components/Header/HeaderBox.js
+++ src/components/Header/HeaderBox.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
 export default class HeaderBox extends React.Component {
-  constructor (props) {
-    super(props)
-    this.props = props
-  }
+  // constructor (props) {
+  //   super(props)
+  //   this.props = props
+  // }
 
   render () {
     return (
@@ -19,7 +19,20 @@ export default class HeaderBox extends React.Component {
   }
 
   getText () {
-    return this.props.link != null ? <a href={this.props.link}><h3 style={{ border: '2px solid white', padding: '10px' }}>{this.props.text}</h3></a> : <h3>{this.props.text}</h3>
+    return this.props.link != null ?
+    <a
+      href={this.props.link}
+    >
+      <h3
+        style={{
+          border: '2px solid white',
+          padding: '10px',
+          margin: '0px'
+        }}
+      >
+        {this.props.text}
+      </h3>
+    </a> : <h3>{this.props.text}</h3>
   }
 }
   
