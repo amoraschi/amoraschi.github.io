2c467ebb45d008d20c43534e7f1cc63879a67c53
Angelo Moraschi <68395930+amoraschi@users.noreply.github.com>
2024-10-10 09:23:05 +0200

    Update page

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,13 @@
+export default function Projects () {
+  return (
+    <main
+      className='grid place-items-center h-[100vh]'
+    >
+      <span
+        className='text-white'
+      >
+        Under construction...
+      </span>
+    </main>
+  )
+}
