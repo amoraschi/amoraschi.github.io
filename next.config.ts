5482bfaf109fdec8013c92e1183a17ac18a5eb3d
Angelo Moraschi <68395930+amoraschi@users.noreply.github.com>
2025-06-10 19:33:29 +0200

    Finish adding projects

--- next.config.ts
+++ next.config.ts
@@ -2,4 +2,10 @@ import nextra from 'nextra'
 
 const withNextra = nextra({})
 
-export default withNextra({})
+export default withNextra({
+  images: {
+    remotePatterns: [
+      new URL('https://avatars.githubusercontent.com/**')
+    ]
+  }
+})
