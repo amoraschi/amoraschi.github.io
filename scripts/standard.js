455ddc2381bce9c80edb7d19ecee5ff2f9bd230e
Angelo Moraschi <68395930+amoraschi@users.noreply.github.com>
2025-12-13 13:16:54 +0100

    Test

--- scripts/standard.js
+++ scripts/standard.js
@@ -25,11 +25,11 @@ const trigger = (env.GITHUB_SHA || '').trim()
 const sha = trigger ? trigger.slice(0, 7) : 'unknown'
 
 execSync(`git checkout -B ${branch}`)
-execSync('git rebase master')
 execSync('git config user.email "github-actions[bot]@users.noreply.github.com"')
 execSync('git config user.name "github-actions[bot]"')
 execSync('git add .')
 execSync('git commit -m "Apply ts-standard --fix"')
+execSync('git rebase master')
 execSync(`git push origin ${branch}`)
 
 const {
