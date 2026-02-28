fc5b06c2db2179df961312dde3c63b95af53d733
Angelo Sho Moraschi <68395930+amoraschi@users.noreply.github.com>
2026-02-28 18:30:50 +0100

    Implement projects page

---
+++ src/components/projects/project.tsx
@@ -0,0 +1,15 @@
+interface ProjectProps {
+  image: string
+  name: string
+  description: string
+}
+
+export default function Project ({
+  image,
+  name,
+  description
+}: ProjectProps) {
+  return (
+    <div></div>
+  )
+}
