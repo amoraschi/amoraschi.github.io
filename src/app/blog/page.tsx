758ad603b8f903e26915b476f73a23ccd1aad3c0
Angelo Sho Moraschi <68395930+amoraschi@users.noreply.github.com>
2026-03-03 11:15:25 +0100

    Start implementing blog page

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,13 @@
+export default function BlogPage () {
+  return (
+    <main
+      className='min-h-screen flex justify-center py-32 px-4'
+    >
+      <p
+        className='text-2xl text-secondary'
+      >
+        In Progress
+      </p>
+    </main>
+  )
+}
--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,6 +1,7 @@
 export const links = [
   'home',
   'projects',
+  'blog',
   'about'
 ]
 
