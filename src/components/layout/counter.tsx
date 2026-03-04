80c8cf754077e6272a67a5721a722be36b33d8ea
Angelo Sho Moraschi <68395930+amoraschi@users.noreply.github.com>
2026-03-04 16:42:59 +0100

    Change dev URL

--- src/components/layout/counter.tsx
+++ src/components/layout/counter.tsx
@@ -3,7 +3,7 @@ import { getHits } from '@/utils/hits'
 export default async function Counter () {
   const dev = process.env.NODE_ENV === 'development'
   const response = await fetch(
-    `https://hits.sh/github.com/${dev ? 'test' : 'amoraschi'}.svg`,
+    `https://hits.sh/${dev ? 'test.com' : 'amoraschi.github.io'}.svg`,
     { next: { revalidate: 3600 } }
   )
 
